b73953cd4a6fbdfe1354a50dece5b7c71e7663a7
Daniel Garcia <daniel@sintrafico.com>
2017-03-22 12:15:45 -0600

    Changed imports to general. Will add extends to leaflet directly

--- src/ST.js
+++ src/ST.js
@@ -1,55 +1,13 @@
 'use strict';
-//require('leaflet');
-var {Map, map} = require('./ST.Map.js');
-var {WMS, wms} = require('./ST.Layer.WMS.js');
-var {Heat, heat} = require('./ST.Layer.Heat.js');
-var {layer} = require('./ST.Layer.js');
-var {Speed, speed} = require('./ST.Layer.WMS.Speed.js');
-var {SpeedLive, speedLive} = require('./ST.Layer.WMS.SpeedLive.js');
-var {Service, service} = require('./ST.Layer.WMS.Service.js');
-var {Municipalities, municipalities} = require('./ST.Layer.Municipalities.js')
-var {ExcessYear, excessYear} = require('./ST.Layer.WMS.ExcessYear.js')
-var {Legend, legend} = require('./ST.Control.Legend.js');
-var {Layers, layers} = require('./ST.Control.Layers.js');
-var {Pois, pois} = require('./ST.Control.Layers.Pois.js')
-var {Incident, incident} = require('./ST.Control.Layers.Incident.js')
-var {Congestion, congestion} = require('./ST.Layer.Heat.Congestion.js');
-var {ToggleButton, toggleButton} = require('./ST.Control.ToggleButton.js');
-
-L.ST = {
-    map: map,
-    Map: Map,
-    Control: {
-        incident: incident,
-        Incident: Incident,
-        pois: pois,
-        Pois: Pois,
-        // General
-        Layers: Layers,
-        legend: legend,
-        Legend: Legend,
-        layers: layers,
-        toggleButton: toggleButton,
-        ToggleButton: ToggleButton,
-    },
-    Layer: {
-        layer: layer,
-        excessYear: excessYear,
-        ExcessYear: ExcessYear,
-        speed: speed,
-        Speed: Speed,
-        speedLive: speedLive,
-        SpeedLive: SpeedLive,
-        service: service,
-        Service: Service,
-        municipalities: municipalities,
-        Municipalities: Municipalities,
-        congestion: congestion,
-        Congestion: Congestion,
-        // General
-        heat: heat,
-        Heat: Heat,
-        wms: wms,
-        WMS: WMS,
-    }
-};
+// Layers
+require('./ST.Layer.js');
+require('./ST.Layer.WMS.js');
+require('./ST.Layer.Heat.js');
+require('./ST.Layer.Municipalities.js')
+// ST Controls
+require('./ST.Control.Layers.js');
+require('./ST.Control.Layers.Pois.js')
+require('./ST.Control.Layers.Incident.js')
+// General Controls
+require('./ST.Control.Legend.js');
+require('./ST.Control.ToggleButton.js');
